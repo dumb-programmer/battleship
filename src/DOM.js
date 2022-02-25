7dfffd240e3e6496a62a5b9dfbd1f8aa27d32be3
dumb-programmer <asadkhan6192@gmail.com>
2022-02-25 12:23:22 +0500

    Add and implement player form functionality

--- src/DOM.js
+++ src/DOM.js
@@ -1,5 +1,6 @@
 import PlayerGameBoardDOM from "./Player_GameBoard_DOM";
 import GameDOM from "./Game_DOM";
+import Player_Form_DOM from "./Player_Form_DOM";
 
 class DOM {
   constructor(game) {
@@ -7,8 +8,12 @@ class DOM {
   }
   render() {
     let gameDOM = GameDOM(this.game);
-    let playerGameBoardDom = PlayerGameBoardDOM(this.game.getPlayerBoard(),gameDOM);
-    playerGameBoardDom.render();
+    let playerGameBoardDom = PlayerGameBoardDOM(
+      this.game.getPlayerBoard(),
+      gameDOM
+    );
+    let playerFormDOM = Player_Form_DOM(this.game, playerGameBoardDom);
+    playerFormDOM.render();
   }
 }
 
---
+++ src/Player_Form_DOM.js
@@ -0,0 +1,54 @@
+import Player from "./Player";
+import PlayerGameBoardDOM from "./Player_GameBoard_DOM";
+
+function Player_Form_DOM(game, Player_GameBoard_DOM) {
+  function renderComponents() {
+    const form = document.createElement("form");
+
+    const label = document.createElement("label");
+    label.innerHTML = "Enter Player Name : <br>";
+    label.setAttribute("for", "name");
+
+    const input = document.createElement("input");
+    input.setAttribute("id", "name");
+    input.setAttribute("type", "text");
+    input.setAttribute("required", "");
+    input.setAttribute("maxlength", "40");
+
+    const btn = document.createElement("button");
+    btn.innerText = "Submit";
+    btn.setAttribute("id", "player-submit-btn");
+    const br = document.createElement("br");
+
+    label.appendChild(input);
+
+    form.appendChild(label);
+    form.appendChild(br);
+    form.appendChild(btn);
+
+    const container = document.createElement("div");
+    container.classList.add("form-container");
+    container.appendChild(form);
+
+    document.body.appendChild(container);
+  }
+
+  function addEventListeners() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input").value;
+      game.setPlayer(new Player(input));
+      Player_GameBoard_DOM.render();
+    });
+  }
+
+  function render() {
+    renderComponents();
+    addEventListeners();
+  }
+
+  return { render };
+}
+
+export default Player_Form_DOM;
